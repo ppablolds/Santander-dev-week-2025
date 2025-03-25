4b36b6f8f6c9aaaf260fff850263278140359401
ppablolds <pablosilvaaluno@outlook.com>
2025-03-25 16:50:17 -0300

    Initial commit

---
+++ src/main/java/me/dio/BradescoDevWeek2025Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BradescoDevWeek2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BradescoDevWeek2025Application.class, args);
+	}
+
+}
